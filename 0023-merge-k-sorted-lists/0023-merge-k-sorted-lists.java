bbfcaded47aebdcdd398e1bbfee89b19768787d5
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2026-07-27 20:13:53 +0530

    Time: 1 ms (99.93%), Space: 46.9 MB (46.46%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,14 +10,53 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] l) {
+
+        //APPROACH !:-
+        // if(l.length==0) return null;
+        // ListNode a=l[0];
+        // for(int i=1;i<l.length;i++){
+        //     ListNode merge=new ListNode(-1);
+        //     ListNode m=merge;
+        //     ListNode b=l[i];
+        //     while(a!=null && b!=null){
+        //     if(a.val>b.val){
+        //         m.next=b;
+        //         m=m.next;
+        //         b=b.next;
+        //     }
+        //     else{
+        //         m.next=a;
+        //         m=m.next;
+        //         a=a.next;
+        //     }
+        //     }
+        //     while(a!=null){
+        //         m.next=a;
+        //         m=m.next;
+        //         a=a.next;
+        //     }
+        //     while(b!=null){
+        //         m.next=b;
+        //         m=m.next;
+        //         b=b.next;
+        //     }
+        //     a=merge.next;
+        // }
+        // return a;
+
+        //APPROACH 2:-
         if(l.length==0) return null;
-
-        ListNode a=l[0];
-
-        for(int i=1;i<l.length;i++){
-            ListNode merge=new ListNode(-1);
-            ListNode m=merge;
-            ListNode b=l[i];
+        int left=0;
+        int right=l.length-1;
+        return fun(left,right,l);
+    }
+    public ListNode fun(int left,int right,ListNode[] arr){
+        if(left==right) return arr[left];
+        int mid=(left+right)/2;
+        ListNode a=fun(left,mid,arr);
+        ListNode b=fun(mid+1,right,arr);
+        ListNode merge=new ListNode(-1);
+        ListNode m=merge;
             while(a!=null && b!=null){
             if(a.val>b.val){
                 m.next=b;
@@ -40,8 +79,7 @@ class Solution {
                 m=m.next;
                 b=b.next;
             }
-            a=merge.next;
-        }
-        return a;
+            return merge.next;
     }
+
 }
