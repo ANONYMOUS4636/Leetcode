796f8f3f10999b6407d97ec25572c4655529d079
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2026-08-09 10:36:29 +0530

    Time: 3 ms (100%), Space: 175.4 MB (70.57%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,23 +1,42 @@
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int[] p=new int[nums.length/2];
+//         int[] n=new int[nums.length/2];
+//         int po=0,ne=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]<0) p[po++]=nums[i];
+//             else n[ne++]=nums[i];
+//         }
+//         ne=0;po=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(i%2!=0){
+//                 nums[i]=p[po++];
+//             }
+//             else{
+//                 nums[i]=n[ne++];
+//             }
+//         }
+
+//         return nums;
+        
+//     }
+// }
+
 class Solution {
-    public int[] rearrangeArray(int[] nums) {
-        int[] p=new int[nums.length/2];
-        int[] n=new int[nums.length/2];
-        int po=0,ne=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<0) p[po++]=nums[i];
-            else n[ne++]=nums[i];
-        }
-        ne=0;po=0;
-        for(int i=0;i<nums.length;i++){
-            if(i%2!=0){
-                nums[i]=p[po++];
+    public int[] rearrangeArray(int[] a) {
+        int n = a.length;
+        int pos=0,neg=1;
+        int b[] = new int[n];
+        for(int i=0;i<n;i++){
+            if(a[i] > 0){
+                b[pos] = a[i];
+                pos += 2;
             }
             else{
-                nums[i]=n[ne++];
+                b[neg] = a[i];
+                neg += 2;
             }
         }
-
-        return nums;
-        
+        return b;
     }
 }
